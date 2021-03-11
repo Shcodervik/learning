c647679df2bcd060903829848c5c50ec6fdba9ec
vmikhina <viktoriia.mikhina@t-systems.com>
2021-03-11 16:19:16 +0300

    L-4 Updating of hero is implemented

--- learning-backend/src/main/java/com/vmikhina/learning/controller/MainController.java
+++ learning-backend/src/main/java/com/vmikhina/learning/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import java.util.NoSuchElementException;
 
 @RestController
 @CrossOrigin
@@ -25,9 +25,23 @@ public class MainController {
     return ResponseEntity.ok(heroes);
   }
   @GetMapping("/hero/{id}")
-  public ResponseEntity hero(@PathVariable("id") int id){
-    Hero hero = repository.findById(id);
-    return ResponseEntity.ok(hero);
+  public ResponseEntity hero(@PathVariable("id") String id){
+    if (repository.findById(id).isPresent()) {
+      Hero hero = repository.findById(id).get();
+      return ResponseEntity.ok(hero);
+    }
+    throw new NoSuchElementException("Data not found");
+  }
+  @GetMapping("/update/hero/{id}/{name}/{title}")
+  public ResponseEntity updateHero(@PathVariable("id") String id, @PathVariable("name") String name, @PathVariable("title") String title) {
+    if (repository.findById(id).isPresent()){
+      Hero hero = repository.findById(id).get();
+      hero.name = name;
+      hero.title = title;
+      repository.save(hero);
+      return ResponseEntity.ok(hero);
+    }
+    throw new NoSuchElementException("Data not found");
   }
 
 }
